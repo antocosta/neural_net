49888e728a3a67f7ec8e466b41ef5e0420a47062
Antonio Sergio Costa <costa.antoniossergio@gmail.com>
2019-02-25 20:08:58 +0100

    neural_net first commit
    first commit

---
+++ neural.py
@@ -0,0 +1,21 @@
+import numpy
+
+
+def nn(m1, m2, w1, w2, b):
+    z = m1 * w1 + m2 * w2 + b
+    return sigmoid(z)
+
+
+def sigmoid(x):
+    return 1/(1 + numpy.exp(-x))
+
+
+def main():
+
+    w1 = numpy.random.randn()
+    w2 = numpy.random.randn()
+    b = numpy.random.randn()
+
+
+main()
+
